ae8f3fe2e45d4514ca872d87bc2b5a009d9dea17
Alok Tiwari <alokkumar.tiwari@cognizant.com>
2019-09-02 23:34:27 +0530

    Final Build

--- src/main/java/com/fse/projectmanagerservice/ProjectManagerServiceApplication.java
+++ src/main/java/com/fse/projectmanagerservice/ProjectManagerServiceApplication.java
@@ -2,12 +2,18 @@ package com.fse.projectmanagerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ProjectManagerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagerServiceApplication.class, args);
+}
+
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ProjectManagerServiceApplication.class);
 	}
 
 }
